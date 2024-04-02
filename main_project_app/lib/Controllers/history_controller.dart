3aae6f924a76b7230f417f48a1740763cc5340a1
Delwin George <delwingeorge21@gmail.com>
2024-04-03 03:09:06 +0530

    updated app name and icon

--- main_project_app/lib/Controllers/history_controller.dart
+++ main_project_app/lib/Controllers/history_controller.dart
@@ -19,11 +19,13 @@ class HistoryController extends GetxController {
 
   List<VibValue> xValues = <VibValue>[];
   List<VibValue> yValues = <VibValue>[];
+  List<VibValue> zValues = <VibValue>[];
 
   RxList<LineSeries<VibValue, int>> XLineSeries =
       <LineSeries<VibValue, int>>[].obs;
   late ChartSeriesController _xSeriesController;
   late ChartSeriesController _ySeriesController;
+  late ChartSeriesController _zSeriesController;
 
   @override
   onInit() {
@@ -39,6 +41,7 @@ class HistoryController extends GetxController {
         for (var i = 0; i < dataList.length; i++) {
           xValues.add(VibValue(value: dataList[i]['x'], index: i));
           yValues.add(VibValue(value: dataList[i]['y'], index: i));
+          zValues.add(VibValue(value: dataList[i]['z'], index: i));
         }
         for (var y in yValues) {
           print("Y: ${y.value}");
@@ -57,6 +60,13 @@ class HistoryController extends GetxController {
             dataSource: yValues,
             xValueMapper: (VibValue yIndexValues, _) => yIndexValues.index,
             yValueMapper: (VibValue yVibValues, _) => yVibValues.value));
+        XLineSeries.add(LineSeries<VibValue, int>(
+            onRendererCreated: (ChartSeriesController controller) {
+              _zSeriesController = controller;
+            },
+            dataSource: zValues,
+            xValueMapper: (VibValue zIndexValues, _) => zIndexValues.index,
+            yValueMapper: (VibValue zVibValues, _) => zVibValues.value));
         update();
         connectToRealTimeDB();
       },
@@ -84,14 +94,20 @@ class HistoryController extends GetxController {
     print(xValues.length);
     xValues.add(VibValue(value: data['x'], index: xValues.last.index + 1));
     yValues.add(VibValue(value: data['y'], index: yValues.last.index + 1));
+    zValues.add(VibValue(value: data['z'], index: zValues.last.index + 1));
     xValues.removeAt(0);
     yValues.removeAt(0);
+    zValues.removeAt(0);
     _xSeriesController.updateDataSource(
       addedDataIndex: xValues.length - 1,
       removedDataIndex: 0,
     );
     _ySeriesController.updateDataSource(
       addedDataIndex: yValues.length - 1,
+      removedDataIndex: 0,
+    );
+    _zSeriesController.updateDataSource(
+      addedDataIndex: zValues.length - 1,
       removedDataIndex: 0,
     );
     xValues.forEach((e) {
