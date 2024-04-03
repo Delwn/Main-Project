0fdc4bbd2d1aa08fa5f3ce307b3c06095d8a7958
delwn <delwingeorge21@gmail.com>
2024-04-04 03:21:07 +0530

    created analysis functionality

--- microprocessor_codes/sensor.py
+++ microprocessor_codes/sensor.py
@@ -9,7 +9,7 @@ import firebase_admin
 import joblib;
 import numpy as np
 import pandas as pd
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, messaging
 import RPi.GPIO as GPIO
 
 # Register
@@ -47,6 +47,24 @@ def get_y_rotation(x,y,z):
 def get_x_rotation(x,y,z):
     radians = math.atan2(y, dist(x,z))
     return math.degrees(radians)
+    
+def send_push_notification(server_key, device_tokens, title, body, data=None):
+    #cred = credentials.Certificate(server_key)
+    #firebase_admin.initialize_app(cred)
+
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(
+            title=title,
+            body=body
+        ),
+        tokens=device_tokens
+    )
+    if data:
+        message.data = data
+
+    response = messaging.send_multicast(message)
+
+    print("Successfully sent notification:", response)
 
 bus = smbus.SMBus(1)
 address = 0x68       # via i2cdetect
@@ -59,6 +77,30 @@ cred = credentials.Certificate("firebase-api.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client() #init firebase
 collection_name_2 = "vibdata"
+analysis_ref = db.collection("History").document("analysis")
+analysis_doc = analysis_ref.get()
+if analysis_doc.exists:
+        a_doc = analysis_doc.to_dict()
+        print(f"Document data: {a_doc['alltime']}")
+
+alltime=a_doc['alltime']
+session=0
+temp_count=0
+start=time.time()
+interval=60
+fpm = 0
+peak=a_doc['peak']
+
+server_key = 'firebase-api.json'
+device_tokens = [
+    'dfDq4NahSy-sdME-0yvh-1:APA91bGRQ-hz3KhRqgW58ptdJvTJNwCrVdcUPwXCQbzOHluLVwk7QGt2koVvt-Nvejsn8ZBs-pUqsFonS2WlJb8NWicGhGty0Otp6_OHgPg4BFOlDi_gbpiBS1MhjAtwK3ixz0nhrUPQ',]
+title = 'Fault Detected'
+body = 'A Fault was detected.'
+data = {
+    'key1': 'value1',
+    'key2': 'value2'
+}
+once = True
 
 # Start the bus to send request for data.
 bus.write_byte_data(address, power_mgmt_1, 0)
@@ -106,9 +148,28 @@ while True:
         inp=np.array([[acceleration_x_scaled,acceleration_y_scaled, acceleration_z_scaled]])
         inp = (inp-m)/sd
         pred = model.predict(inp)
-        pred =0
         if pred > 0.5:
                 status = True;
+                if once:
+                        send_push_notification(server_key, device_tokens, title, body, data)
+                        once = False
+                alltime+=1
+                session+=1
+                temp_count+=1
+                if time.time()-start>=interval:
+                    fpm=temp_count
+                    if fpm > peak:
+                            peak = fpm
+                    temp_count=0
+                    start=time.time()
+                analysis = {
+                "alltime" : alltime,
+                "session" : session,
+                "peak" : peak,
+                "current" : fpm
+                }
+                print("here")
+                db.collection("History").document("analysis").set(analysis)
                 #GPIO.output(in1, False)
         else:
                 status = False;
